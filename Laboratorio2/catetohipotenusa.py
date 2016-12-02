52eb605b68dda14ba8197d29fc3a8afce54420d3
katyajared <katyajared94@gmail.com>
2016-12-01 22:10:47 -0600

    Add files via upload

---
+++ Laboratorio2/catetohipotenusa.py
@@ -0,0 +1,8 @@
+#Katya Jared Amador Rodriguez
+#25/11/2016
+a=input("Calcularemos la hipotenusa del triangulo que dessees, favor de ingresar tu cateto a: ")
+b=input("Ingresa el cateto b: ")
+import math
+hip=a**2+b**2
+raiz=math.sqrt(hip)
+print "La hipotenusa del triangulo que ingresaste es: " , raiz
---
+++ Laboratorio2/tarifataxi.py
@@ -0,0 +1,13 @@
+#Katya Jared Amador Rodriguez
+#25/11/2016
+def calcula(km):
+    m=km*1000
+    tarifa=8.74
+    distancia=(m/250)*1.84+8.74
+    #print "Por la distancia que quiere recorrer, ustede debe pagar: ", distancia, "pesos"
+    return distancia
+#print "Hola, a continuacion te diremos cuanto debes pagar por un taxi dependiendo de los km que vayas a recorrer"
+#km=input("Ingresa los km que desees recorrer: ")
+#m=km*1000
+#distancia=(m/250)*1.84
+#print "Por la distancia que quiere recorrer, ustede debe pagar: ", distancia, "pesos"
